da6af79cbe033f00b4125a93b45e6b5ee1cfd48c
BuschMath <busch@iowacentral.edu>
2023-02-02 09:44:13 -0600

    first commit

---
+++ ActualClass.java
@@ -0,0 +1,7 @@
+public class ActualClass {
+    public TestClass thing;
+
+    ActualClass(){
+        thing = new TestClass();
+    }
+}
---
+++ TestClass.java
@@ -0,0 +1,8 @@
+public class TestClass{
+    public int num1;
+    public int num2;
+
+    public void method(){
+        System.out.print("TestOutput");
+    }
+}
